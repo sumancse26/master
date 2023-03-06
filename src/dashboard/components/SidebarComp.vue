309778a61f80411e507c25610858b0547ceaaab6
suman <suman.iu32@gmail.com>
2023-03-06 17:53:33 +0600

    Category

--- src/dashboard/components/SidebarComp.vue
+++ src/dashboard/components/SidebarComp.vue
@@ -7,7 +7,33 @@
 		<span class="material-symbols-outlined"> av_timer </span>
 		<h5>Dashboard</h5>
 	</div>
-	<div class="create-category"></div>
+
+	<div class="accordion accordion-flush sidebar-accordion" id="accordionFlushExample">
+		<div class="accordion-item">
+			<h2 class="accordion-header accordion-header-custom" id="flush-headingOne">
+				<button
+					class="accordion-button collapsed accordion-header-custom"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#flush-collapseOne"
+					aria-expanded="false"
+					aria-controls="flush-collapseOne"
+				>
+					Category List
+				</button>
+			</h2>
+			<div
+				id="flush-collapseOne"
+				class="accordion-collapse collapse category-name"
+				aria-labelledby="flush-headingOne"
+				data-bs-parent="#accordionFlushExample"
+			>
+				<div class="accordion-body">
+					<p class="m-0 p-0">Category Name</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
